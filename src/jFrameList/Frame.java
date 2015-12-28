2131697753c87bf1c87b081c72fe947cf30ab85b
Sourec <tothemyers@gmail.com>
2015-12-28 17:02:21 -0500

    Add Frame data storage class

---
+++ src/jFrameList/Frame.java
@@ -0,0 +1,12 @@
+package jFrameList;
+
+import java.awt.List;
+
+
+class Frame
+{
+	public String name;
+	public int year;
+	public List researchers = new List();
+	public List foci = new List();
+}
