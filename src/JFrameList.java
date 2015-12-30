e64b8bea790c4bba829de7838dc75df8534d4f44
Sourec <tothemyers@gmail.com>
2015-12-29 22:03:36 -0500

    Moved FRAME_List.xml to src

--- src/JFrameList.java
+++ src/JFrameList.java
@@ -4,13 +4,12 @@ import jFrameList.*;
 
 public class JFrameList
 {
-	static final String encoding = "UTF-8";
 	public static void main(String[] args)
 	{
 		String filename = new String("FRAME_List.xml");
 		FrameLibrary flib = new FrameLibrary();
 		flib.parse(filename);
-		ArrayList<Frame> temp = flib.searchFocus("Culture");
+		ArrayList<Frame> temp = flib.searchFocus("Memory");
 		for (int i = 0; i < temp.size(); i++)
 		{
 			System.out.println(temp.get(i).name);
